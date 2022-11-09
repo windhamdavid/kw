844d8d66c32360b4db30902d3d82407d60d89094
windhamdavid <david@davidawindham.com>
2022-11-09 14:12:42 -0500

    adding 🎧

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,12 @@
-
-
 Amplitude.init({
 	"songs": [
+		{
+			"name": "Scenes from Childhood, Op. 15 No. 1",
+			"artist": "Robert Schumann",
+			"album": "",
+			"url": "/audio/schumman.mp3",
+			"cover_art_url": "/img/schumann.png"
+		},
 		{
 			"name": "Mazurka in A minor, Op. 17 No. 4",
 			"artist": "Frédéric Chopin",
@@ -14,7 +19,7 @@ Amplitude.init({
 			"artist": "Claude Debussy",
 			"album": "Children's Corner Suite, L 113",
 			"url": "/audio/dr_gradus.m4a",
-			"cover_art_url": "/img/gradus.png"
+			"cover_art_url": "/img/gradus.jpg"
 		},
 		{
 			"name": "Sonatina in C Major, Op. 55 No. 1",
@@ -107,6 +112,111 @@ Amplitude.init({
 			"url": "/audio/diversions/1_clementisample.mp3",
 			"cover_art_url": "/img/diversions.jpg"
 		},
+		{
+			"name": "Clementi Sample #2",
+			"artist": "2010",
+			"album": "Diversions",
+			"url": "/audio/diversions/2_Clementisample2.mp3",
+			"cover_art_url": "/img/diversions.jpg"
+		},
+		{
+			"name": "Nocture Sample",
+			"artist": "2010",
+			"album": "Diversions",
+			"url": "/audio/diversions/3_nocturnesample.mp3",
+			"cover_art_url": "/img/diversions.jpg"
+		},
+		{
+			"name": "Diversion in D Sharp Minor",
+			"artist": "2010",
+			"album": "Diversions",
+			"url": "/audio/diversions/4_diversion_in_dsharpm.mp3",
+			"cover_art_url": "/img/diversions.jpg"
+		},
+		{
+			"name": "Meditation in F Sharp",
+			"artist": "2010",
+			"album": "Diversions",
+			"url": "/audio/diversions/mediation_in_fsharp.mp3",
+			"cover_art_url": "/img/diversions.jpg"
+		},
+		{
+			"name": "Poem #2",
+			"artist": "2010",
+			"album": "Diversions",
+			"url": "/audio/diversions/poemnumber2.mp3",
+			"cover_art_url": "/img/diversions.jpg"
+		},
+		{
+			"name": "Prelude in C Minor",
+			"artist": "2010",
+			"album": "Diversions",
+			"url": "/audio/diversions/Prelude_in_Cm1.mp3",
+			"cover_art_url": "/img/diversions.jpg"
+		},
+		{
+			"name": "Solfeggio",
+			"artist": "2010",
+			"album": "Diversions",
+			"url": "/audio/diversions/solfeggio.mp3",
+			"cover_art_url": "/img/diversions.jpg"
+		},
+		{
+			"name": "Waltz in E Minor",
+			"artist": "2010",
+			"album": "Diversions",
+			"url": "/audio/diversions/waltz_in_em.mp3",
+			"cover_art_url": "/img/diversions.jpg"
+		},
+		{
+			"name": "Protos",
+			"artist": "2015",
+			"album": "Mystic",
+			"url": "/audio/mystic/1-Protos.mp3",
+			"cover_art_url": "/img/mystic.png"
+		},
+		{
+			"name": "Mercury",
+			"artist": "2015",
+			"album": "Mystic",
+			"url": "/audio/mystic/2-Mercury.mp3",
+			"cover_art_url": "/img/mystic.png"
+		},
+		{
+			"name": "Mystic",
+			"artist": "2015",
+			"album": "Mystic",
+			"url": "/audio/mystic/3-Mystic.mp3",
+			"cover_art_url": "/img/mystic.png"
+		},
+		{
+			"name": "Hermes",
+			"artist": "2015",
+			"album": "Mystic",
+			"url": "/audio/mystic/4-Hermes.mp3",
+			"cover_art_url": "/img/mystic.png"
+		},
+		{
+			"name": "Agentum",
+			"artist": "2015",
+			"album": "Mystic",
+			"url": "/audio/mystic/5-Agentum.mp3",
+			"cover_art_url": "/img/mystic.png"
+		},
+		{
+			"name": "Aurum",
+			"artist": "2015",
+			"album": "Mystic",
+			"url": "/audio/mystic/6-Aurum.mp3",
+			"cover_art_url": "/img/mystic.png"
+		},
+		{
+			"name": "Ouroboros",
+			"artist": "2015",
+			"album": "Mystic",
+			"url": "/audio/mystic/7-Ouroboros.mp3",
+			"cover_art_url": "/img/mystic.png"
+		},
 	],
   "callbacks": {
         'play': function(){
@@ -164,9 +274,12 @@ for( var i = 0; i < songElements.length; i++ ){
 
 const myModalEl = document.getElementById('Video');
 var video = document.getElementById("redemption");
+var video2 = document.getElementById("hollow");
 function stopVideo() {
 	video.pause();
 	video.currentTime = 0;
+	video2.pause();
+	video2.currentTime = 0;
 }
 myModalEl.addEventListener('hidden.bs.modal', event => {
   stopVideo();
